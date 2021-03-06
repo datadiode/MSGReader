034a8353d33dad18108f7cd56f981f911c63757a
datadiode <datadiode@users.noreply.github.com>
2021-03-06 17:18:52 +0100

    experimental

--- MsgReaderCore/Mime/MessagePart.cs
+++ MsgReaderCore/Mime/MessagePart.cs
@@ -571,7 +571,7 @@ namespace MsgReader.Mime
 		/// <returns>The <see cref="Body"/> property as a string</returns>
 		public string GetBodyAsText()
 		{
-			return BodyEncoding.GetString(Body);
+			return Encoding.ASCII.GetString(Body); //BodyEncoding.GetString(Body);
 		}
         #endregion
 
